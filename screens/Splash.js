8f1da96822a52dbb898e314a2fc667f128b09915
Akshay Pandit <panditakshay402@gmail.com>
2024-08-14 02:14:32 +0530

    logo added and splash screen timeout code added to home.js

--- screens/Splash.js
+++ screens/Splash.js
@@ -1,12 +1,50 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import {View, Text} from 'react-native';
+import React, { useEffect } from 'react';
+import * as Animatable from 'react-native-animatable';
+import logo from '../assets/images/logo3.png';
+import {Colors} from 'react-native/Libraries/NewAppScreen';
+import {useNavigation} from '@react-navigation/native';
 
 const Splash = () => {
-  return (
-    <View>
-      <Text>Splash</Text>
-    </View>
-  )
-}
+const navigation = useNavigation();
+useEffect(() => {
+    setTimeout(() => {
+        navigation.navigate('Home');
+    }, 3000);
+}, []);
 
-export default Splash
+  return (
+      <View style={styles.container}>
+        <Animatable.Image
+          animation={'bounceIn'}
+          source={logo}
+          style={styles.logo}
+        />
+        <Animatable.Text animation={'slideInUp'} style={styles.text}>
+          Recipe
+        </Animatable.Text>
+      </View>
+  );
+};
+const styles = {
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'white',
+  },
+
+  text: {
+    fontSize: 46,
+    color: 'black',
+    fontWeight: 'bold',
+    marginTop: 20,
+  },
+  logo: {
+    width: 200,
+    height: 200,
+    // borderRadius: 100,
+  },
+};
+
+export default Splash;
