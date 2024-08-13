24fa0b97633b82dd03d948da383afe7a7af53909
Akshay Pandit <panditakshay402@gmail.com>
2024-08-14 00:57:22 +0530

    navigation setup

--- App.js
+++ App.js
@@ -1,11 +1,10 @@
 import {View, Text} from 'react-native';
 import React from 'react';
+import AppNavigator from './screens/AppNavigator';
 
 const App = () => {
   return (
-    <View>
-      <Text>App</Text>
-    </View>
+    <AppNavigator />
   )
 }
 
---
+++ screens/AppNavigator.js
@@ -0,0 +1,20 @@
+import { View, Text } from 'react-native';
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import Home from './Home';
+
+const Stack = createStackNavigator()
+const AppNavigator = () => {
+  return (
+    <NavigationContainer>
+        <Stack.Navigator>
+            <Stack.Screen name="Home" component={Home} />
+        </Stack.Navigator>  
+    </NavigationContainer>
+
+    
+  )
+}
+
+export default AppNavigator
