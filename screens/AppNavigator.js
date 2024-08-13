42743fc3f5436d3b3b232af0a2eb19fbf6448cd7
Akshay Pandit <panditakshay402@gmail.com>
2024-08-14 01:28:05 +0530

    screens added

--- screens/AppNavigator.js
+++ screens/AppNavigator.js
@@ -1,20 +1,40 @@
-import { View, Text } from 'react-native';
+import {View, Text} from 'react-native';
 import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
 import Home from './Home';
+import Search from './Search';
+import Details from './Details';
+import Splash from './Splash';
 
-const Stack = createStackNavigator()
+const Stack = createStackNavigator();
 const AppNavigator = () => {
   return (
     <NavigationContainer>
-        <Stack.Navigator>
-            <Stack.Screen name="Home" component={Home} />
-        </Stack.Navigator>  
+      <Stack.Navigator>
+      <Stack.Screen
+          name="Splash"
+          component={Splash}
+          options={{headerShown: false}}
+        />
+        <Stack.Screen
+          name="Home"
+          component={Home}
+          options={{headerShown: false}}
+        />
+        <Stack.Screen
+          name="Search"
+          component={Search}
+          options={{headerShown: false}}
+        />
+        <Stack.Screen
+          name="Details"
+          component={Details}
+          options={{headerShown: false}}
+        />
+      </Stack.Navigator>
     </NavigationContainer>
+  );
+};
 
-    
-  )
-}
-
-export default AppNavigator
+export default AppNavigator;
